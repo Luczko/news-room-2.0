7bd88cb4f92a02b44bc50b54a3ac2bb09e0f0863
Luczko <wlalukasz@gmail.com>
2021-06-23 22:06:31 +0200

    readmeAndcssChanges

--- README.md
+++ README.md
@@ -1,27 +1,27 @@
-# NewsRoom
+# NewsRoom - Serie A News
+
+Prosty projekt front-endowy strony z newsami z włoskiej Serie-A.
+Dodawaj, usuwaj i głosuj na najlepsze newsy!
+
+Simple front-end project - website with Italian Serie-A news.
+Add, delete and vote for best news!
+
+## Funkcjonalności / Funkcjonalities
+
+- dodawanie newsów za pomocą chowanego formularza z obecną datą
+- usuwanie newsów
+- oddawanie głosów na najlepsze newsy
+- ranking najbardziej popularnych newsów
+
+- adding news with current date with responsive forms
+- deleting news
+- voting for the best news
+- ranking of most popular news
+
+## Development info
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.1.
 
-## Development server
-
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+# Have fun, and check my other projects!
--- src/app/news.ts
+++ src/app/news.ts
@@ -9,28 +9,22 @@ export interface News {
 export const NEWS: News[] = [
   {
     title: 'Milan poza czwórką. Rossoneri polegli w Rzymie',
-    news:
-      'AC Milan jeszcze niedawno był uważany za głównego rywala Interu Mediolan w walce o mistrzostwo Włoch, a po poniedziałkowym meczu 33. kolejki z Lazio wypadł poza pierwszą piątkę. Rossoneri w wyjazdowym spotkaniu na Stadio Olimpico przegrali 0:3.',
-    photoUrl:
-      'https://www.seriea.pl/wp-content/uploads/2021/04/immobile-correa-768x512.jpg',
+    news: 'AC Milan jeszcze niedawno był uważany za głównego rywala Interu Mediolan w walce o mistrzostwo Włoch, a po poniedziałkowym meczu 33. kolejki z Lazio wypadł poza pierwszą piątkę. Rossoneri w wyjazdowym spotkaniu na Stadio Olimpico przegrali 0:3.',
+    photoUrl: 'assets/images/news2.jpg',
     votes: 0,
     date: '2021-04-26',
   },
   {
     title: 'Atalanta bez litości dla Bolonii. Pracowity mecz Skorupskiego',
-    news:
-      'Atalanta wysoko rozbiła u siebie Bolonię (5:0). Po pierwszej połowie La Dea prowadziła już 2:0, a gdy po przerwie czerwoną kartkę obejrzał Jerdy Schouten, tylko dopełnili formalności.',
-    photoUrl:
-      'https://www.seriea.pl/wp-content/uploads/2021/04/muriel-luis-1-768x512.jpg',
+    news: 'Atalanta wysoko rozbiła u siebie Bolonię (5:0). Po pierwszej połowie La Dea prowadziła już 2:0, a gdy po przerwie czerwoną kartkę obejrzał Jerdy Schouten, tylko dopełnili formalności.',
+    photoUrl: 'assets/images/news3.jpg',
     votes: 0,
     date: '2021-04-25',
   },
   {
     title: 'Inter wykonał kolejny krok w kierunku tytułu',
-    news:
-      'Inter Mediolan zgodnie z planem pokonał na swoim stadionie Hellas Weronę. 18-krotni mistrzowie Włoch zanotowali jednocześnie 14. triumf na swoim obiekcie.',
-    photoUrl:
-      'https://www.seriea.pl/wp-content/uploads/2021/04/darmian-matteo-768x512.jpg',
+    news: 'Inter Mediolan zgodnie z planem pokonał na swoim stadionie Hellas Weronę. 18-krotni mistrzowie Włoch zanotowali jednocześnie 14. triumf na swoim obiekcie.',
+    photoUrl: 'assets/images/news1.jpg',
     votes: 0,
     date: '2021-04-25',
   },
